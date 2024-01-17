6891a6f1907896e036ac336e5bd00449a7d2826e
tahataha005 <taha.f.taha0@gmail.com>
2024-01-17 23:35:38 +0200

    Create <> Routes for auth methods

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -2,12 +2,18 @@ import { RouteGroup } from "@config/contants/types/route.types";
 import { bodySchemaBuilder } from "@config/utils/requests/request";
 
 import { loginSchema, LoginDto } from "@features/auth/schemas/login.req.schema";
-import { login } from "@features/auth";
+import { login, register } from "@features/auth";
+import { RegisterDto, registerSchema } from "./schemas/register.req";
 
 export const authRoutes: RouteGroup = (app, options, done) => {
   app.post("/login", {
     handler: login,
     schema: bodySchemaBuilder<LoginDto>(loginSchema),
+  });
+
+  app.post("/register", {
+    handler: register,
+    schema: bodySchemaBuilder<RegisterDto>(registerSchema),
   });
 
   done();
