c5a3ceba9a4f24fe2bad5eb57ee16d2eceb8a583
tahataha005 <taha.f.taha0@gmail.com>
2024-01-17 19:08:13 +0200

    Create <> Body schema builder

---
+++ src/config/utils/requests/request.ts
@@ -0,0 +1,11 @@
+import { BodySchema } from "@config/contants/types/request.type";
+import { OBJECT } from "@config/contants/variables/contants.variables";
+
+export const bodySchemaBuilder = <T>(schema: BodySchema<T>) => {
+  return {
+    body: {
+      ...schema,
+      type: OBJECT,
+    },
+  };
+};
