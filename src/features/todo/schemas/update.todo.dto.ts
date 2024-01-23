e288636d441e4c59b5380c58469beb6a9e791274
tahataha005 <taha.f.taha0@gmail.com>
2024-01-23 13:32:06 +0200

    Create <> Update todo dto

---
+++ src/features/todo/schemas/update.todo.dto.ts
@@ -0,0 +1,20 @@
+import { BodySchema } from "@config/contants/types/request.type";
+
+export type UpdateTodoDto = {
+  title?: string;
+  description?: string;
+  time?: string;
+  score?: number;
+  completed?: boolean;
+};
+
+export const updateTodoSchema: BodySchema<UpdateTodoDto> = {
+  properties: {
+    title: { type: "string", minLength: 1 },
+    description: { type: "string", minLength: 1 },
+    time: { type: "string" },
+    completed: { type: "boolean" },
+    score: { type: "number" },
+  },
+  required: [],
+};
