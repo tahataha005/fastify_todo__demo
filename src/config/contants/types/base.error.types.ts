bed207f1ebd194b7c2c84ba493b608d28bd145bd
tahataha005 <taha.f.taha0@gmail.com>
2024-01-23 09:12:05 +0200

    Add <> Reply to base error params types

--- src/config/contants/types/base.error.types.ts
+++ src/config/contants/types/base.error.types.ts
@@ -1,6 +1,9 @@
+import { FastifyReply } from "fastify";
+
 export interface BaseErrorParams {
   message?: string;
   errorCheck?: boolean;
+  reply: FastifyReply;
 }
 
 export interface NotFoundParams extends BaseErrorParams {
