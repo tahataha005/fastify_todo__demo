878c8f5ced611736d96d0080a81137f86ab88611
tahataha005 <taha.f.taha0@gmail.com>
2024-01-23 09:38:44 +0200

    Create <> Get todo method

---
+++ src/features/todo/todo.controller.ts
@@ -0,0 +1,24 @@
+import { ControllerMethod } from "@config/contants/types/controller.type";
+import { IdParam } from "@config/utils/requests/request";
+import Todo from "./models/todo.model";
+import { throwNotFound } from "@config/utils/errors/errors";
+
+export const getTodo: ControllerMethod = async (request, reply) => {
+  const { id } = request.params as IdParam;
+  const { user } = request;
+
+  if (id) {
+    const todo = await Todo.findUnique({ where: { id: parseFloat(id) } });
+
+    throwNotFound({
+      entity: "Todo",
+      reply,
+    });
+
+    return reply.send(todo);
+  } else {
+    const todos = await Todo.findMany({ where: { userId: user!.id } });
+
+    return reply.send(todos);
+  }
+};
