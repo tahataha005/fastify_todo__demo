631225410a881321b8bf1d55b91fb3e2363113db
tahataha005 <taha.f.taha0@gmail.com>
2024-01-23 13:31:42 +0200

    Add <> Score to create todo

--- src/features/todo/schemas/create.todo.dto.ts
+++ src/features/todo/schemas/create.todo.dto.ts
@@ -4,6 +4,7 @@ export type CreateTodoDto = {
   title: string;
   description: string;
   time: string;
+  score: number;
 };
 
 export const createTodoSchema: BodySchema<CreateTodoDto> = {
@@ -11,6 +12,7 @@ export const createTodoSchema: BodySchema<CreateTodoDto> = {
     title: { type: "string", minLength: 1 },
     description: { type: "string", minLength: 1 },
     time: { type: "string" },
+    score: { type: "number" },
   },
   required: ["title", "time"],
 };
