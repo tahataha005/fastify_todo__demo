d3b9d5f8dc097b593ded0b420630e2528bf51e36
tahataha005 <taha.f.taha0@gmail.com>
2024-01-23 09:32:50 +0200

    Create <> Auth middleware

---
+++ src/config/settings/middlewares/auth.middleware.ts
@@ -0,0 +1,67 @@
+import { ControllerMethod } from "@config/contants/types/controller.type";
+import { throwUnauthorized } from "@config/utils/errors/errors";
+import jwt from "jsonwebtoken";
+import User from "@features/auth/models/user.model";
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from "fastify";
+import { MiddlewareMethod } from "@config/contants/types/middleware.type";
+
+type CustomPayload = {
+  id: number;
+  email: string;
+};
+
+export const authMiddleware: MiddlewareMethod = async (
+  request,
+  reply,
+  done
+) => {
+  const { authorization } = request.headers;
+
+  throwUnauthorized({
+    reply,
+    errorCheck: !authorization,
+  });
+
+  const splitted = authorization!.split(" ");
+
+  throwUnauthorized({
+    reply,
+    errorCheck: splitted.length !== 2,
+  });
+
+  const [type, token] = splitted;
+
+  throwUnauthorized({
+    reply,
+    errorCheck: type !== "Bearer",
+  });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as CustomPayload;
+
+    throwUnauthorized({
+      reply,
+      errorCheck: !decoded,
+    });
+
+    const user = await User.findUnique({
+      where: {
+        id: decoded.id,
+      },
+    });
+
+    throwUnauthorized({
+      reply,
+      errorCheck: !user,
+    });
+
+    request.user = user;
+
+    done();
+  } catch (error) {
+    throwUnauthorized({
+      reply,
+      message: "Invalid token",
+    });
+  }
+};
