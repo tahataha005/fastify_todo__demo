7ef4d92c09834e98c2ae34d4874172c2599cad9e
tahataha005 <taha.f.taha0@gmail.com>
2024-01-23 13:33:04 +0200

    Create <> Update todo method

--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -2,6 +2,9 @@ import { ControllerMethod } from "@config/contants/types/controller.type";
 import { IdParamSchema } from "@config/contants/types/request.type";
 import { throwNotFound } from "@config/utils/errors/errors";
 import Todo from "@features/todo";
+import { CreateTodoDto } from "./schemas/create.todo.dto";
+import { UpdateTodoDto } from "./schemas/update.todo.dto";
+import { calculateScoreIfUpdated } from "./todo.service";
 
 export const getTodo: ControllerMethod = async (request, reply) => {
   const { id } = request.params as IdParamSchema;
@@ -25,22 +28,58 @@ export const getTodo: ControllerMethod = async (request, reply) => {
 
 export const createTodo: ControllerMethod = async (request, reply) => {
   const { user } = request;
-  const { title, description, time } = request.body as {
-    title: string;
-    description: string;
-    time: string;
-  };
+  const { title, description, time, score } = request.body as CreateTodoDto;
 
   const todo = await Todo.create({
     data: {
       title,
       description,
       time,
-      score: 0,
+      score,
       completed: false,
       userId: user!.id,
     },
   });
+
+  return reply.send(todo);
+};
+
+export const updateTodo: ControllerMethod = async (request, reply) => {
+  const { id } = request.params as IdParamSchema;
+  const { user } = request;
+  const { title, description, time, score, completed } =
+    request.body as UpdateTodoDto;
+
+  const old = await Todo.findUnique({
+    where: {
+      id: parseFloat(id),
+      userId: user!.id,
+    },
+  });
+
+  throwNotFound({
+    errorCheck: !old,
+    entity: "Todo",
+    reply,
+  });
+
+  const todo = await Todo.update({
+    where: {
+      id: parseFloat(id),
+      userId: user!.id,
+    },
+    data: {
+      title,
+      description,
+      time,
+      score,
+      completed,
+    },
+  });
+
+  if (old!.score !== score || old!.completed !== completed) {
+    await calculateScoreIfUpdated(user!);
+  }
 
   return reply.send(todo);
 };
