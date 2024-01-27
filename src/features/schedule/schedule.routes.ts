9cfcdc0e4aff0d4463a0de6c23e0d47051847606
tahataha005 <taha.f.taha0@gmail.com>
2024-01-28 01:09:28 +0200

    Create <> Assign route to update Schedule

--- src/features/schedule/schedule.routes.ts
+++ src/features/schedule/schedule.routes.ts
@@ -2,13 +2,16 @@ import { RouteGroup } from "../../config/contants/types/route.types";
 import {
   IdParam,
   bodySchemaBuilder,
+  paramsSchemaBuilder,
 } from "../../config/utils/requests/request";
-import { createSchedule, getSchedules } from "./schedule.controller";
+import { createSchedule, getSchedules, updateSchedule } from "./schedule.controller";
 import { authMiddleware } from "../../config/settings/middlewares/auth.middleware";
 import {
   CreateScheduleDto,
   createScheduleSchema,
 } from "./schemas/create.schedule.dto";
+import { IdParamSchema } from '@config/contants/types/request.type';
+import { UpdateScheduleDto, updateScheduleSchema } from './schemas/update.schedule.dto';
 
 export const scheduleRoutes: RouteGroup = (app, options, done) => {
   app.get("/:id?", {
@@ -20,6 +23,16 @@ export const scheduleRoutes: RouteGroup = (app, options, done) => {
   app.post("/", {
     schema: bodySchemaBuilder<CreateScheduleDto>(createScheduleSchema),
     handler: createSchedule,
+    preHandler: [authMiddleware],
+  });
+
+  app.put("/:id", {
+    schema: {
+      ...paramsSchemaBuilder<IdParamSchema>(IdParam.params),
+      ...bodySchemaBuilder<UpdateScheduleDto>(updateScheduleSchema),
+
+    },
+    handler: updateSchedule,
     preHandler: [authMiddleware],
   });
 
