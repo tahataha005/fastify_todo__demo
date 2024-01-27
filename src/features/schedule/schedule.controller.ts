8a818662b5c5cefca7801436d8fcb1f34e5fe0a7
tahataha005 <taha.f.taha0@gmail.com>
2024-01-28 00:43:43 +0200

    Create <> Create schedule method

--- src/features/schedule/schedule.controller.ts
+++ src/features/schedule/schedule.controller.ts
@@ -1,6 +1,8 @@
 import { ControllerMethod } from "../../config/contants/types/controller.type";
 import { IdParamSchema } from "../../config/contants/types/request.type";
 import { throwNotFound } from "../../config/utils/errors/errors";
+import Schedule from "./models/schedule.model";
+import { CreateScheduleDto } from "./schemas/create.schedule.dto";
 
 export const getSchedules: ControllerMethod = async (request, reply) => {
   const { id } = request.params as IdParamSchema;
@@ -23,4 +25,21 @@ export const getSchedules: ControllerMethod = async (request, reply) => {
   } else {
     return reply.send(schedules);
   }
+};
+
+export const createSchedule: ControllerMethod = async (request, reply) => {
+  const { day, todos } = request.body as CreateScheduleDto;
+  const { user } = request;
+
+  const schedule = await Schedule.create({
+    data: {
+      day,
+      todos: {
+        connect: todos.map((todo) => ({ id: todo })),
+      },
+      userId: user!.id,
+    },
+  });
+
+  return reply.send(schedule);
 };
