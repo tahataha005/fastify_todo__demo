6c4687ce17d7d9e6eda8530f0f050009f74fd0a8
tahataha005 <taha.f.taha0@gmail.com>
2024-01-28 00:44:05 +0200

    Create <> Route for create schedule

--- src/features/schedule/schedule.routes.ts
+++ src/features/schedule/schedule.routes.ts
@@ -1,12 +1,25 @@
 import { RouteGroup } from "../../config/contants/types/route.types";
-import { IdParam } from "../../config/utils/requests/request";
-import { getSchedules } from "./schedule.controller";
+import {
+  IdParam,
+  bodySchemaBuilder,
+} from "../../config/utils/requests/request";
+import { createSchedule, getSchedules } from "./schedule.controller";
 import { authMiddleware } from "../../config/settings/middlewares/auth.middleware";
+import {
+  CreateScheduleDto,
+  createScheduleSchema,
+} from "./schemas/create.schedule.dto";
 
 export const scheduleRoutes: RouteGroup = (app, options, done) => {
   app.get("/:id?", {
     schema: IdParam,
     handler: getSchedules,
+    preHandler: [authMiddleware],
+  });
+
+  app.post("/", {
+    schema: bodySchemaBuilder<CreateScheduleDto>(createScheduleSchema),
+    handler: createSchedule,
     preHandler: [authMiddleware],
   });
 
