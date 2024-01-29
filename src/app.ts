7628aa092110496b66afc63eba687af321ecf156
tahataha005 <taha.f.taha0@gmail.com>
2024-01-29 14:57:47 +0200

    Config <> Edit app configs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
+import dotenv from "dotenv";
 import sensible from "@fastify/sensible";
 import fastify from "fastify";
+import { welcomeRoute } from "./config/contants/variables/welcome.route";
+import { errorHandler } from "./config/utils/errors/global.error.handler";
+
+dotenv.config({
+  path: "src/config/settings/enviroments/.env.development",
+});
 
 const app = fastify();
 
+app.route({
+  ...welcomeRoute,
+});
+
 app.register(sensible);
+
+app.setErrorHandler(errorHandler);
 
 export default app;
