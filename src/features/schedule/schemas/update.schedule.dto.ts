d3c60fa4c9159ceb537f13c20b7efa07ad7b86e9
tahataha005 <taha.f.taha0@gmail.com>
2024-01-29 14:55:48 +0200

    Add <> Deleted todos in update schedule dto

--- src/features/schedule/schemas/update.schedule.dto.ts
+++ src/features/schedule/schemas/update.schedule.dto.ts
@@ -1,14 +1,16 @@
-import { BodySchema } from '@config/contants/types/request.type';
+import { BodySchema } from "@config/contants/types/request.type";
 
 export type UpdateScheduleDto = {
   day: Date;
-  todos: number[]
+  todos: number[];
+  toDelete: number[];
 };
 
 export const updateScheduleSchema: BodySchema<UpdateScheduleDto> = {
   properties: {
     day: { type: "string" },
     todos: { type: "array", items: { type: "number" } },
+    toDelete: { type: "array", items: { type: "number" } },
   },
   required: [],
 };
