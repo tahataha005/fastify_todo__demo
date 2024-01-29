d700a3a5a80314a24cc33537d4da4f498745938d
tahataha005 <taha.f.taha0@gmail.com>
2024-01-29 14:58:20 +0200

    Edit <> INdex configs

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
-import dotenv from "dotenv";
 import app from "./app";
+
 import { registerRouteGroup } from "./config/utils/routes/route";
 import { authRoutes } from "./features/auth";
 import { todoRoutes } from "./features/todo";
-import { welcomeRoute } from "./config/contants/variables/welcome.route";
 import { scheduleRoutes } from "./features/schedule";
-
-dotenv.config({
-  path: "src/config/settings/enviroments/.env.development",
-});
 
 registerRouteGroup("/auth", authRoutes);
 registerRouteGroup("/todo", todoRoutes);
 registerRouteGroup("/schedule", scheduleRoutes);
-
-app.route({
-  ...welcomeRoute,
-});
 
 app.listen(
   {
